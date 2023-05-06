8dcbfd2fb894410b7699ca9b7f448e0b4270e719
DavyKing <yungdynamic53@gmail.com>
2023-05-06 16:56:50 +0100

    creating an exchange for acala using chopsticks

--- contracts/Trader.sol
+++ contracts/Trader.sol
@@ -9,15 +9,15 @@ import "@acala-network/contracts/utils/AcalaTokens.sol";
 contract Trader {
     constructor() payable {}
 
-    function trade() external returns (bool) {
-        uint256 price = IOracle(ORACLE).getPrice(ACA);
+    function trade(address tokenA, address tokenB) external returns (bool) {
+        uint256 price = IOracle(ORACLE).getPrice(tokenA);
         if (price < 1e18) {
             revert("price too low");
         }
 
         address[] memory path = new address[](2);
-        path[0] = ACA;
-        path[1] = AUSD;
+        path[0] = tokenA;
+        path[1] = tokenB;
         uint256 amount = address(this).balance / 1_000_000;
         return IDEX(DEX).swapWithExactSupply(path, amount, 0);
     }
